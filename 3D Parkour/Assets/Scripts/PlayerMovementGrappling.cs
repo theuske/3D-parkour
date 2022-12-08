87ea296dd3819e28927a8f43221bdd2ac5e4b670
theuske <justintheus15@gmail.com>
2022-12-08 12:32:41 +0100

    Push 6
    PUSHHH

---
+++ 3D Parkour/Assets/Scripts/PlayerMovementGrappling.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+internal class PlayerMovementGrappling
+{
+    internal bool freeze;
+
+    internal void JumpToPosition(Vector3 grapplePoint, float highestPointOnArc)
+    {
+        throw new NotImplementedException();
+    }
+}
