5fea0e426d35783205ad4db201911f834f6cbea6
theuske <justintheus15@gmail.com>
2022-12-08 12:30:30 +0100

    Update 2
    Update 2

---
+++ 3D Parkour/Assets/Scripts/DragObject.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PushBox : MonoBehaviour
+{
+    public float speed = 2f;
+    public Rigidbody box;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            box.AddForce(transform.forward * speed);
+        }
+    }
+}
